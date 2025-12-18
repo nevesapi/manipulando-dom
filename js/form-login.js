34e8f41332fdacb60f4ce870778c1a33d0745645
Denis Neves <98157939+nevesapi@users.noreply.github.com>
2025-12-17 22:16:25 -0300

    Finaliza login e inicia faq

--- js/form-login.js
+++ js/form-login.js
@@ -3,6 +3,7 @@ const usuario = document.getElementById("email");
 const password = document.getElementById("password");
 const btnLogin = document.getElementById("btn-login");
 const mensagem = document.querySelector("#mensagem");
+// const section = document.querySelector(".form-container")
 
 // Usuário fixo - apenas ficticio para estudo
 const usuarioCorreto = "exemplo@exemplo.com";
@@ -26,6 +27,8 @@ btnLogin.addEventListener("click", (event) => {
     infoMensagem("Login realizado com sucesso!", "green");
 
     // salvando o login do usuário
+    localStorage.setItem("usuarioLogado", usuarioValue);
+    localStorage.setItem("logado", "true");
   } else {
     infoMensagem("Usuário ou senha incorretos!", "red");
   }
@@ -35,3 +38,27 @@ function infoMensagem(text, color) {
   mensagem.style.color = color;
   mensagem.innerHTML = text;
 }
+
+window.addEventListener("load", () => {
+  const logado = localStorage.getItem("logado");
+
+  if (logado === "true") {
+    infoMensagem("Usuário já está logado", "green");
+    // redirecionando o usuário para a página inicial
+    // window.location.href = "/index.html";
+
+    const btnLogout = document.createElement("button");
+    btnLogout.classList.add("btn-logout");
+    btnLogout.innerHTML = "Sair";
+    // section.appendChild(btnLogout)
+
+    btnLogout.addEventListener("click", function () {
+      localStorage.removeItem("logado");
+      localStorage.removeItem("usuarioLogado");
+      infoMensagem("", "");
+      window.location.href = "/";
+    });
+
+    mensagem.insertAdjacentElement("afterend", btnLogout);
+  }
+});
